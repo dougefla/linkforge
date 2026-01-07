0e80499089cf9b895d0a15150c6bd30c3a6ebf65
arounamounchili <virtual.power.virtual@gmail.com>
2026-01-07 21:07:41 +0100

    docs: sync documentation version and add automation hint

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,9 @@ sys.path.insert(0, os.path.abspath("../.."))
 project = "LinkForge"
 copyright = "2025, Arouna Patouossa Mounchili"
 author = "Arouna Patouossa Mounchili"
-release = "1.0.0"
+# The full version, including alpha/beta/rc tags
+# version: 1.1.0
+release = "1.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
